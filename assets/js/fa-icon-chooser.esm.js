84bd7e94ece13ff24f86cb311ec5ee14748984cd
Justin Kruit <justinkruitnl@gmail.com>
2025-12-22 15:56:21 +0100

    Add close button to popup

--- assets/js/fa-icon-chooser.esm.js
+++ assets/js/fa-icon-chooser.esm.js
@@ -140,7 +140,7 @@ import 'https://cdn.jsdelivr.net/npm/@fortawesome/fa-icon-chooser@0.10.0-2/dist/
         <div class="acffa-icon-chooser-card">
 			<div class="title-wrapper">
 				<h2>Choose an Icon</h2>
-				<button type="button" class="acffa-icon-chooser-close button-link"><?php esc_html_e( 'Close', 'acf-font-awesome' ); ?></button>
+				<button type="button" class="acffa-icon-chooser-close button-link"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z"/></svg></button>
 			</div>
 			<div class="chooser-wrapper"></div>
         </div>
@@ -152,9 +152,14 @@ import 'https://cdn.jsdelivr.net/npm/@fortawesome/fa-icon-chooser@0.10.0-2/dist/
   }
   insertIconChooserContainer();
 
-  // when clicking outside the card, close the chooser
+  // when clicking outside the card or on the close button, close the chooser
   container.on('click', function (e) {
     if ($(e.target).is('#acffa-icon-chooser-container')) {
+      container.removeClass('open');
+      container.find('fa-icon-chooser').remove();
+    }
+
+    if ($(e.target).closest('.acffa-icon-chooser-close').length) {
       container.removeClass('open');
       container.find('fa-icon-chooser').remove();
     }
